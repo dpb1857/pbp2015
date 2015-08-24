271569538cbf1c34969a083cd5546b242b74070f
Don Bennett <dpb@donbennett.org>
2015-08-24 15:11:39 -0700

    Start pbp2015 scrapers.

---
+++ riders.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+import requests
+
+
+# req = requests.post("http://suivi.paris-brest-paris.org/index.php", data={"rch":"1", "rch_num":"5", "rch_nom":""})
+req = requests.post("http://suivi.paris-brest-paris.org/index.php", data={"rch":"1", "rch_num":"5"})
+import pdb; pdb.set_trace()
+pass
