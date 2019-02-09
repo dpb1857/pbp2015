2374cf79855639219c342b4b2a7072a7e75a7d75
Don Bennett <dpb@donbennett.org>
2019-02-09 01:18:54 -0800

    checkpoint

---
+++ pbpdata/data/models.py
@@ -0,0 +1,33 @@
+
+# Find pylint codes at: http://pylint-messages.wikidot.com/all-codes
+# pylint: disable=line-too-long
+# pylint: disable=invalid-name
+# pylint: disable=missing-docstring
+# pylint: disable=too-few-public-methods
+# pylint: disable=superfluous-parens
+
+from django.db import models
+
+# Create your models here.
+
+class Rider(models.Model):
+    plaque = models.CharField(max_length=4)
+    firstname = models.CharField(max_length=22)
+    lastname = models.CharField(max_length=20)
+    gender = models.CharField(max_length=1)
+    country = models.CharField(max_length=2)
+    club = models.CharField(max_length=39)
+    velo = models.CharField(max_length=2)
+    time = models.CharField(max_length=5)
+
+
+class Control(models.Model):
+    seq = models.IntegerField()
+    name = models.CharField(max_length=20)
+    distance = models.IntegerField()
+    inbound = models.BooleanField()
+
+class Timestamp(models.Model):
+    plaque = models.CharField(max_length=4)
+    location = models.ForeignKey('Control', on_delete=models.CASCADE)
+    timestamp = models.IntegerField()
---
+++ pbpdata/data/util.py
@@ -0,0 +1,75 @@
+
+# Find pylint codes at: http://pylint-messages.wikidot.com/all-codes
+# pylint: disable=line-too-long
+# pylint: disable=invalid-name
+# pylint: disable=missing-docstring
+# pylint: disable=too-few-public-methods
+# pylint: disable=superfluous-parens
+
+import json
+
+from django.db import transaction
+
+from data.models import Rider, Control, Timestamp
+
+def load_rider_data():
+    f = open("../data/finalresults.json")
+    count = 0
+    with transaction.atomic():
+        for record in f:
+            r = json.loads(record)
+            r = Rider(**r)
+            r.save()
+            count += 1
+
+    print("Added {} Rider records".format(count))
+
+
+locations = [(1, "SQY", 0, False),
+             (2, "Vilaines-la-Juhel", 136, False),
+             (3, "Fougeres", 191, False),
+             (4, "Tinteniac", 224, False),
+             (5, "Loudeac", 277, False),
+             (6, "St-Nicholas-du-Pelem", 305, False),
+             (7, "Carhaix", 327, False),
+             (8, "Brest", 381, False),
+             (9, "Carhaix", 434, True),
+             (10, "Mael-Carhaix", 440, True),
+             (11, "Loudeac", 486, True),
+             (12, "Tinteniac", 539, True),
+             (13, "Fougeres", 572, True),
+             (14, "Vilaines-la-Juhel", 627, True),
+             (15, "Mortagne-au-Perche", 678, True),
+             (16, "Dreux", 726, True),
+             (17, "SQY", 765, True)]
+
+def load_control_locations():
+    count = 0
+    with transaction.atomic():
+        for seq, name, distance, inbound in locations:
+            c = Control(seq=seq, name=name, distance=distance, inbound=inbound)
+            c.save()
+            count += 1
+
+    print("Added {} Control records".format(count))
+
+
+def load_timestamps():
+    f = open("../data/results.json")
+    count = 0
+
+    controls = list(Control.objects.all())
+    controls.sort(key=lambda x: x.seq)
+    with transaction.atomic():
+        for record in f:
+            r = json.loads(record)
+            plaque = r["plaque"]
+            results = r["results"]
+            results = results[:5] + results[15:16] + results[5:8] + results[16:17] + results[8:16]
+            for control, result in zip(controls, results):
+                t = Timestamp(plaque=plaque, location=control, timestamp=42)
+                t.save()
+
+            count += 1
+
+    print("Added {} Timestamp records".format(count))
