4b1e91e58a9cc82dc7dfd0284182a957fe0fdb19
Don Bennett <dpb@donbennett.org>
2019-02-23 00:27:26 -0800

    Use database urls.

--- pbpdata/pbpdata/settings.py
+++ pbpdata/pbpdata/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
+import dj_database_url
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -76,19 +78,23 @@ WSGI_APPLICATION = 'pbpdata.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    ## 'default': {
-    ##     'ENGINE': 'django.db.backends.sqlite3',
-    ##     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    ## }
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'pbp2015',
-        'USER': 'pbp2015',
-        'PASSWORD': 'pbp2015',
-        'HOST': 'thinkpad',
-        'PORT': 5432
-        }
+##     ## 'default': {
+##     ##     'ENGINE': 'django.db.backends.sqlite3',
+##     ##     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+##     ## }
+##     'default': {
+##         'ENGINE': 'django.db.backends.postgresql',
+##         'NAME': 'pbp2015',
+##         'USER': 'pbp2015',
+##         'PASSWORD': 'pbp2015',
+##         'HOST': 'thinkpad',
+##         'PORT': 5432
+##         }
 }
+
+DATABASES["default"] = dj_database_url.parse(os.environ["DATABASE_URL"], conn_max_age=600)
+_ = os.environ["PGUSER"]
+_ = os.environ["PGPASSWORD"]
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
